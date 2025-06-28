68fd11967d2b59ddf4d327711c58bc83b5e161e9
LordTerry <216334295+LordTerry@users.noreply.github.com>
2025-06-28 19:56:21 -0300

    Update bdscan_utils.lua

--- bdscan_utils.lua
+++ bdscan_utils.lua
@@ -4,8 +4,11 @@ local HONEYPOT_NAMES = {"Kick", "kick", "Ban", "ban", "KickClient", "BanClient",
                         "kick_client", "ban_client", "kick_remote", "ban_remote", "Kick_Client", "Ban_Client",
                         "Kick_Remote", "Ban_Remote", "Admin", "admin", "AntiCheat", "anticheat", "anti_cheat",
                         "Anti_Cheat", "BanPlayer", "KickPlayer", "Ban_Player", "Kick_Player", "kick_player",
-                        "ban_player". "BanPlayers", "KickPlayers", "Ban_Players", "Kick_Players", "kick_players",
-                        "ban_players"}
+                        "ban_player", "BanPlayers", "KickPlayers", "Ban_Players", "Kick_Players", "kick_players",
+                        "ban_players", "BanAnticheat", "KickAnticheat", "BanAntiCheat", "KickAntiCheat", 
+                        "Ban_AntiCheat", "Kick_AntiCheat", "ban_AntiCheat", "kick_AntiCheat", "ban_anticheat",
+                        "kick_anticheat", "Ban_Anti_Cheat", "Kick_Anti_Cheat", "kick_Anti_Cheat", "ban_Anti_Cheat",
+                        "kick_anti_cheat", "ban_anti_cheat"}
 
 function __FILTER__HANDLER__(v: Instance)
   if v:IsA("RemoteEvent") or v:IsA("RemoteFunction") and not (v.Name == "__FUNCTION" or v.Parent.Name == "__FUNCTION" or v:FindFirstChild("__FUNCTION")) then
