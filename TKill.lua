17673345d95b5cfb5837e2a2036213f91dcfd86e
LordTerry <216334295+LordTerry@users.noreply.github.com>
2025-07-29 04:06:40 -0300

    Create TKill.lua
    yer

---
+++ TKill.lua
@@ -0,0 +1,38 @@
+--//LOCAL PLAYER RELATED THINGS
+local PLRS = game.Players
+local PLR = PLRS.LocalPlayer
+local CHAR = PLR.Character or PLR.CharacterAdded:Wait()
+local HRP = CHAR:WaitForChild("HumanoidRootPart")
+local HUM = CHAR:WaitForChild("Humanoid")
+
+--//TARGET?
+local TARGET = PLRS["MegaSkidOfAlISkids"]
+local TCHAR = TARGET.Character or TARGET.CharacterAdded:Wait()
+local THRP = TCHAR:WaitForChild("HumanoidRootPart")
+local THUM = TCHAR:WaitForChild("Humanoid")
+local OLD = THRP.CFrame
+
+--//FUNCS
+function SETTUP(NAME, H)
+  if CHAR[NAME] then
+    CHAR[NAME].Parent = PLR.Backpack
+  end
+  TOOL = PLR.Backpack[NAME]
+  
+  TOOL.GripPos = Vector3.new(-H, 1.5, 0.5)
+  TOOL.Parent = CHAR
+  TOOL.Handle.Name = "_"
+end
+
+--//MAIN CODE IDK
+SETTUP("Sword", 1000)
+for _, v in pairs(TCHAR:GetChildren()) do
+  if v:IsA("BasePart") then
+    v.Size = Vector3.new(50, 50, 50)
+  end
+end
+THRP.CFrame = HRP.CFrame + Vector3.new(0, 999, 0)
+THRP.Anchored = true
+task.wait(1)
+THRP.Anchored = false
+THRP.CFrame = OLD
